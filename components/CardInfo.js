041c8c27452e22098e8eae053e145f6293653ef3
csuau <csuaubou@gmail.com>
2022-10-11 11:58:25 +0200

    settings cardInfo - add props

--- components/CardInfo.js
+++ components/CardInfo.js
@@ -2,30 +2,26 @@ import Link from "next/link";
 import React from "react";
 import Media from "./Media";
 
-const CardInfo = () => {
+const CardInfo = ({title, resum, link, img, color}) => {
     return (
-        <article className="pt-xxxxxl pb-xxl shadow">
-            <header className="bg-secondary d-flex f-direction-row align-items-center p-xxs gap-xxxs">
+        <article className="shadow card">
+            <header className={`bg-${color} d-flex f-direction-row align-items-center p-xxs gap-xs card--header`}
+
+            >
                 <Media
                     loading="lazy"
-                    relativePath="/images/riesgos-laborales.png"
+                    relativePath={img}
                     alt="logo"
                     title="logo centro médico"
                     name="logo centro médico"
                     height="70px"
                     width="70px"
                 />
-                <h2 className="heading--5 color-neutral10">Prevención Riesgos Laborales</h2>
+                <h2 className="heading--5 color-neutral10">{title}</h2>
             </header>
-            <div className="bg-neutral20 p-xs">
-                <p className="body--medium color-neutral50">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod
-                    tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat.</p>
-                <Link href="/riesgos-laborales">
+            <div className="bg-neutral20 p-xs card--body">
+                <p className="body--medium color-neutral50">{resum}</p>
+                <Link href={link}>
                     <a className=" btn btn--neutral50_outline mt-s body--medium">VER MÁS</a>
                 </Link>
             </div>
