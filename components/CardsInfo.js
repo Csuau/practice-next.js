4f6f6a97eae4101a34f51bf6383c71b20aedc725
csuau <csuaubou@gmail.com>
2022-10-19 16:40:42 +0200

    add services page

--- components/CardsInfo.js
+++ components/CardsInfo.js
@@ -6,13 +6,13 @@ const CardsInfo = () => {
             <div className="container grid gap-xs ">
                 <CardInfo title="Prevención Riesgos Laborales"
                           resum="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                          link="/riesgos-laborales"
+                          link="/servicios/riesgos-laborales"
                           img="/images/riesgos-laborales.png"
                           color="secondary"
                 />
                 <CardInfo title="Medicina del trabajo"
                           resum="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                          link="medicina-trabajo"
+                          link="/servicios/medicina-trabajo"
                           img="/images/medicina-trabajo.png"
                           color="tertiary"
                 />
