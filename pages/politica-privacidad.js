2ab7740c1256d26a0b48f0f784215aa7015fb2bc
csuau <csuaubou@gmail.com>
2022-10-19 17:46:24 +0200

    add privacy policy page

--- pages/politica-privacidad.js
+++ pages/politica-privacidad.js
@@ -1,8 +1,20 @@
 import Base from '../Layouts/Base'
+import MainHero from "../components/MainHero";
+import BasicContent from "../components/BasicContent";
+import {fakeText} from "../data/fakeText";
 
-export default function PrivacyPolicy() {
+const PageMetasI18n = {
+    meta_title: 'Centro Médico - Formación',
+    meta_description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    meta_robots: 'index,follow'
+}
+const PrivacyPolicy = () => {
     return (
-        <Base/>
+        <Base PageMetasI18n={PageMetasI18n}>
+            <MainHero/>
+            <BasicContent title='Política de privacidad' text={fakeText}/>
+        </Base>
 
     )
 }
+export default PrivacyPolicy
