f95d06ae0051c12fd14f963049f940882f6f52e1
csuau <csuaubou@gmail.com>
2022-10-19 15:12:48 +0200

    add cards info component

---
+++ components/CardsInfo.js
@@ -0,0 +1,30 @@
+import CardInfo from "./CardInfo";
+
+const CardsInfo = () => {
+    return (
+        <section className=" py-xxl">
+            <div className="container grid gap-xs ">
+                <CardInfo title="Prevención Riesgos Laborales"
+                          resum="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                          link="/riesgos-laborales"
+                          img="/images/riesgos-laborales.png"
+                          color="secondary"
+                />
+                <CardInfo title="Medicina del trabajo"
+                          resum="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                          link="medicina-trabajo"
+                          img="/images/medicina-trabajo.png"
+                          color="tertiary"
+                />
+                <CardInfo title="Formación"
+                          resum="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                          link="formacion"
+                          img="/images/formacion.png"
+                          color="primary"
+                />
+
+            </div>
+        </section>
+    )
+}
+export default CardsInfo
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,15 @@
 import Base from '../Layouts/Base'
 import MainHero from "../components/MainHero";
-import CardInfo from "../components/CardInfo";
+
 import Visor360 from "../components/Visor360";
 import Tabs from "../components/Tabs";
+import CardsInfo from "../components/CardsInfo";
 
 const Home = () => {
     return (
         <Base>
             <MainHero title={'Salud y Trabajo'} subtitle={'EL EQUILIBRIO PERSONAL'}/>
-            <section className=" py-xxl">
-                <div className="container grid gap-xs ">
-                    <CardInfo title="Prevención Riesgos Laborales"
-                              resum="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                              link="/riesgos-laborales"
-                              img="/images/riesgos-laborales.png"
-                              color="secondary"
-                    />
-                    <CardInfo title="Medicina del trabajo"
-                              resum="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                              link="medicina-trabajo"
-                              img="/images/medicina-trabajo.png"
-                              color="tertiary"
-                    />
-                    <CardInfo title="Formación"
-                              resum="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                              link="formacion"
-                              img="/images/formacion.png"
-                              color="primary"
-                    />
-
-                </div>
-            </section>
+            <CardsInfo/>
             <section className=" py-xxl">
                 <div className="container">
                     <Visor360/>
